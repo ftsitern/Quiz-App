6d25bce96447718a475aea0f88ce9182919f2f45
Balabhadra Chand <revolution.ninety9@gmial.com>
2021-06-20 12:13:38 +0530

    All pushed again

---
+++ src/Questions.js
@@ -0,0 +1,22 @@
+const questions =  [
+    {
+        id: 1,
+        question: "Which one of these is not a javascipt framework or library ?",
+        options: ["React", "Angular", "Laravel", "jQuery", "Vue"],
+        correct: "Laravel"
+    },
+    {
+        id: 2,
+        question: "Which one of these is odd one out ?",
+        options: ["MySql", "PostgreSQl", "MariaDB", "Cassandra", "None of these"],
+        correct: "Cassandra"
+    },
+    {
+        id: 3,
+        question: "Which HTTP status code represents conflict response condition ?",
+        options: ["401", "501", "200", "409", "None of these"],
+        correct: "409"
+    }
+]
+
+export default questions;
