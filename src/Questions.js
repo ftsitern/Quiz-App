31e94dae27741ce4e2e894c7911c49c95e8837c5
rajveer singh <rajveermbic@gmail.com>
2021-06-20 20:54:52 +0530

    questions added

--- src/Questions.js
+++ src/Questions.js
@@ -1,22 +1,93 @@
-const questions =  [
-    {
-        id: 1,
-        question: "Which one of these is not a javascipt framework or library ?",
-        options: ["React", "Angular", "Laravel", "jQuery", "Vue"],
-        correct: "Laravel"
-    },
-    {
-        id: 2,
-        question: "Which one of these is odd one out ?",
-        options: ["MySql", "PostgreSQl", "MariaDB", "Cassandra", "None of these"],
-        correct: "Cassandra"
-    },
-    {
-        id: 3,
-        question: "Which HTTP status code represents conflict response condition ?",
-        options: ["401", "501", "200", "409", "None of these"],
-        correct: "409"
-    }
+const questions = [
+  {
+    id: 1,
+    question: 'Which one of these is not a javascipt framework or library ?',
+    options: ['React', 'Angular', 'Laravel', 'jQuery', 'Vue'],
+    correct: 'Laravel',
+  },
+  {
+    id: 2,
+    question: 'Which one of these is odd one out ?',
+    options: ['MySql', 'PostgreSQl', 'MariaDB', 'Cassandra', 'None of these'],
+    correct: 'Cassandra',
+  },
+  {
+    id: 3,
+    question: 'Which HTTP status code represents conflict response condition ?',
+    options: ['401', '501', '200', '409', 'None of these'],
+    correct: '409',
+  },
+  {
+    id: 4,
+    question: 'Which type of JavaScript language is ___',
+    options: [
+      'Object-Oriented',
+      'Object-Based',
+      'Assembly-language',
+      'High-leve',
+    ],
+    correct: 'Object-Based',
+  },
+  {
+    id: 5,
+    question:
+      ' In the JavaScript, which one of the following is not considered as an error:',
+    options: [
+      'Syntax error',
+      'Missing of semicolons',
+      'Division by zero',
+      'Missing of Bracket',
+    ],
+    correct: 'Division by zero',
+  },
+  {
+    id: 6,
+    question:
+      'Which of the following number object function returns the value of the number?',
+    options: ['toString()', 'valueOf()', 'toLocaleString()', 'toPrecision()'],
+    correct: 'valueOf()',
+  },
+  {
+    id: 7,
+    question: 'In JavaScript the x===y statement implies that:',
+    options: [
+      'Both x and y are equal in value, type and reference address as well.',
+      'Both are x and y are equal in value only.',
+      'Both are equal in the value and data type.',
+      'Both are not same at all.',
+    ],
+    correct: 'Both are equal in the value and data type.',
+  },
+  {
+    id: 8,
+    question:
+      'In JavaScript, what will be used for calling the function definition expression:',
+    options: [
+      'Function prototype',
+      'Function literal',
+      'Function calling',
+      'Function declaration',
+    ],
+    correct: 'Function literal',
+  },
+  {
+    id: 9,
+    question:
+      'Which one of the following is used for the calling a function or a method in the JavaScript:',
+    options: [
+      'Property Access Expression',
+      'Functional expression',
+      'Invocation expression',
+      'Primary expression',
+    ],
+    correct: 'Invocation expression',
+  },
+  {
+    id: 10,
+    question: 'Which one of the following is an ternary operator:',
+    options: ['?', ':', '-', '+',],
+    correct: '?',
+  },
 ]
 
 export default questions;
